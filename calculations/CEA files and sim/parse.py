2333a3146744597ed88392d354c5f560e9b3e87d
1998-felix <felix.gateru@gmail.com>
2022-06-22 16:06:50 +0300

    Adding parsing output file

---
+++ calculations/CEA files and sim/parse.py
@@ -0,0 +1,36 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+
+file_name = input("Please input the file name:")
+file = open(file_name).read()
+outputs = file.split('THEORETICAL')
+outputs = outputs[1:]
+df = pd.DataFrame()
+for output in outputs:
+    dic = {}
+    Pin = output.split('Pin =')[1].split('PSIA')[0].strip()
+    O_F = output.split('O/F=')[1].split('%FUEL')[0].strip()
+    Isp = output.split('Isp, M/SEC')[1].split('MASS')[0].strip()
+    dic['Pin'] = float(Pin)
+    dic['O_F'] = float(O_F)
+    dic['Isp'] = float(Isp)
+    df1 = pd.DataFrame(dic, index=[0])
+    df = pd.concat([df, df1], ignore_index=True)
+
+
+Pins = list(df['Pin'].unique())
+colors = ['o-r','o-b','o-g','o-c','o-m','o-y','o-k','o-r','o-b','o-g','o-c','o-m','o-y','o-k']
+cnt = 0
+
+for Pin in Pins:
+    color = colors[cnt]
+    val = df[df['Pin']==Pin]
+    plt.plot(val['O_F'],val['Isp'],color)
+    cnt += 1
+
+plt.legend([str(Pin)+'(psia)' for Pin in Pins])
+plt.xlabel('O/F ratio')
+plt.ylabel('Isp')
+plt.show()
