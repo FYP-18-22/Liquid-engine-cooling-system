82550239cdb56e520234a2b1299ab95a0f5689cc
1998-felix <felix.gateru@gmail.com>
2022-06-23 04:49:34 +0300

    Modifying parsing program

--- calculations/CEA files and sim/parse.py
+++ calculations/CEA files and sim/parse.py
@@ -1,10 +1,12 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import os
+import sys
 
 
 
-file_name = input("Please input the file name:")
-file = open(file_name).read()
+#file_name = input("Please input the file name:")
+file = open(os.path.join(sys.path[0],"output.txt")).read()
 outputs = file.split('THEORETICAL')
 outputs = outputs[1:]
 df = pd.DataFrame()
@@ -12,10 +14,12 @@ for output in outputs:
     dic = {}
     Pin = output.split('Pin =')[1].split('PSIA')[0].strip()
     O_F = output.split('O/F=')[1].split('%FUEL')[0].strip()
-    Isp = output.split('Isp, M/SEC')[1].split('MASS')[0].strip()
+    Isp = output.split('Isp, M/SEC')[1].split('MOLE')[0].strip().split()[0]
     dic['Pin'] = float(Pin)
     dic['O_F'] = float(O_F)
+    print(Isp.split())
     dic['Isp'] = float(Isp)
+    
     df1 = pd.DataFrame(dic, index=[0])
     df = pd.concat([df, df1], ignore_index=True)
 
